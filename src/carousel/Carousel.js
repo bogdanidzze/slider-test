d3d3e8a94234f02abdfbe96e00e6ea8604c86890
Bohdan <106009824+bogdanidzze@users.noreply.github.com>
2022-09-29 08:16:13 +0300

    edit

--- src/carousel/Carousel.js
+++ src/carousel/Carousel.js
@@ -7,6 +7,25 @@ const PAGE_WIDTH = 450
 
 export const Carousel = ({ children }) => {
     const [pages,setPages] = useState([]) 
+    const [offset,setOffset] = useState(0) 
+
+    const handleLeftArrowClick = () => {
+        setOffset((currentOffset) => {
+            const newOffset = currentOffset + PAGE_WIDTH
+            console.log(newOffset)
+            return Math.min(newOffset,0)
+        })
+    }
+
+    const handleRightArrowClick = () => {
+        setOffset((currentOffset) => {
+            const newOffset = currentOffset - PAGE_WIDTH
+            const maxOffset = -(PAGE_WIDTH * (pages.length - 1))
+            console.log(newOffset)
+            return Math.max(newOffset,maxOffset)
+        })
+    }
+
     useEffect(() => {
         setPages(
             Children.map(children, child => {
@@ -23,13 +42,16 @@ export const Carousel = ({ children }) => {
 
     return (
         <div className="main-container">
-        <FaChevronLeft/>
+        <FaChevronLeft className='arrow' onClick={handleLeftArrowClick}/>
             <div className="window">
-                <div className="all-pages-container">
+                <div className="all-pages-container" 
+                style={{
+                    transform: `translateX(${offset}px)`,
+                }}>
                     {pages}
                 </div>
             </div>
-        <FaChevronRight/>
+        <FaChevronRight className='arrow' onClick={handleRightArrowClick}/>
         </div>
     )
 }
